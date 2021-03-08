70c5f73d8ba160db698e2a9fa82c96693f6062aa
Jonas Konrad <jonas.konrad@epfl.ch>
2021-03-08 10:13:04 +0100

    add feature take picture to add to database

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/MainActivity.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.github.HumanLearning2021.HumanLearningApp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchToCameraActivity(view: View) {
+        val intent = Intent(this, AddPictureActivity::class.java)
+        startActivity(intent)
     }
 }
