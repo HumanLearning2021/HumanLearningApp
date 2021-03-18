9ea7806b4435a5a979c340fccebc148b5dcfcc9a
Marouane Jaakik <marouanejaakik@Marouanes-MacBook-Pro.local>
2021-03-18 12:36:04 +0100

    addButton Tests

---
+++ app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/DataCreationActivityTest.kt
@@ -0,0 +1,57 @@
+package com.github.HumanLearning2021.HumanLearningApp
+
+import android.widget.Button
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import org.junit.Rule
+import org.junit.runner.RunWith
+import org.junit.Test
+
+
+
+
+
+@RunWith(AndroidJUnit4::class)
+class DataCreationActivityTestgit  {
+
+    @get:Rule
+    var testRule = ActivityTestRule(DataCreationActivity::class.java)
+
+
+    @Test
+    fun rowViewIsDsiplayedWhenAddButtonIsClicked(){
+        onView(ViewMatchers.withId(R.id.button_add)).perform(ViewActions.click())
+        onView(ViewMatchers.withHint("Enter Category")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+
+
+    }
+    fun rowViewIsAddedWhenAddButtonIsClicked(){
+        onView(ViewMatchers.withId(R.id.button_add)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.parent_linear_layout)).check(ViewAssertions.matches(
+            ViewMatchers.hasChildCount(1)))
+
+
+    }
+
+    fun rowViewIsRemovedWhenRemoveButtonIsClicked(){
+        onView(ViewMatchers.withId(R.id.button_add)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.button_remove)).perform(ViewActions.click())
+        onView(ViewMatchers.withId(R.id.parent_linear_layout)).check(ViewAssertions.matches(
+            ViewMatchers.hasChildCount(0)))
+
+    }
+
+
+
+
+
+
+
+}
