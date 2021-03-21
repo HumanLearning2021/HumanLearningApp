3d8b413b69d6f1e5d84225255e0c23392aa18c9e
Niels Lachat <niels.lachat@epfl.ch>
2021-03-21 10:20:51 +0100

    Integrate the dummy dataset with the learning UI
    Also some refactoring because my separation between view and presenter
    didn't make much sense.
    I will still need to integrate with PR#31 once it's merged.

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/presenter/LearningPresenter.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/presenter/LearningPresenter.kt
@@ -1,86 +1,48 @@
 package com.github.HumanLearning2021.HumanLearningApp.presenter
 
-import android.content.ClipData
-import android.content.ClipDescription
-import android.util.Log
-import android.view.DragEvent
-import android.view.MotionEvent
-import android.view.View
+import android.app.Activity
 import android.widget.ImageView
-import com.github.HumanLearning2021.HumanLearningApp.R
-import kotlin.random.Random
+import com.github.HumanLearning2021.HumanLearningApp.model.Category
+import com.github.HumanLearning2021.HumanLearningApp.model.DatasetInterface
+import com.github.HumanLearning2021.HumanLearningApp.model.DummyCategory
+import com.github.HumanLearning2021.HumanLearningApp.model.DummyDatasetInterface
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 class LearningPresenter {
-    companion object {
-        fun onImageToSortTouched(view: View, event: MotionEvent): Boolean {
-            val clipDataLabel = "My Clip Data"
-            return when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
-                    val item = ClipData.Item(view.contentDescription)
-                    val dragData = ClipData(
-                            clipDataLabel,
-                            arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
-                            item
-                    )
-                    val shadow = View.DragShadowBuilder(view)
-                    view.startDragAndDrop(dragData, shadow, view, 0)
-                    true
-                }
-                else -> false
-            }
-        }
+    private lateinit var categories : Set<Category>
+    private val datasetInterface: DatasetInterface
 
-        val targetOnDragListener = View.OnDragListener { v, event ->
-            val opaque = 1.0f
-            val halfOpaque = opaque / 2
-            when (event.action) {
-                DragEvent.ACTION_DRAG_STARTED -> true
-                DragEvent.ACTION_DRAG_ENTERED -> {
-                    v.alpha = halfOpaque
-                    v.invalidate() // to force redraw
-                    true
-                }
-                DragEvent.ACTION_DRAG_LOCATION -> true
-                DragEvent.ACTION_DRAG_EXITED -> {
-                    v.alpha = opaque
-                    v.invalidate()
-                    true
-                }
-                DragEvent.ACTION_DROP -> {
-                    val item: ClipData.Item = event.clipData.getItemAt(0)
-                    v.alpha = opaque
-                    v.invalidate()
-                    item.text == v.contentDescription
-                }
-                DragEvent.ACTION_DRAG_ENDED -> {
-                    if (event.result) {
-                        when (val iv = event.localState) {
-                            is ImageView -> displayNextPicture(iv)
-                            else -> throw IllegalStateException("The local state of the drag " +
-                                    "and drop should be of type ImageView")
-                        }
-                        true
-                    } else {
-                        false
-                    }
-                }
-                else -> {
-                    Log.e("targetOnDragListener", "Unkown action type")
-                    false
-                }
-            }
-        }
+    constructor(datasetInterface: DatasetInterface, coroutineScope: CoroutineScope) {
+        // load the categories when the presenter is constructed. My idea is that the LearningActivity
+        // will be started with an intent referencing the dataset the person clicked on in a
+        // different screen. So it's ok to load the categories on construction. And to change the
+        // current dataset, the activity will need to be relaunched
 
-        private fun displayNextPicture(view: ImageView) {
-            // TODO this is a dirty hack, replace with model call
-            fun gS(id: Int) = view.context.getString(id)
-            val colorStrings = listOf(gS(R.string.learning_cat_0_descr),
-                    gS(R.string.learning_cat_1_descr), gS(R.string.learning_cat_2_descr))
-            val colorInts = listOf(R.color.red, R.color.green, R.color.blue)
-            val rCol = Random.nextInt(from = 0, until = 2)
-            view.setImageResource(colorInts[rCol])
-            view.contentDescription = colorStrings[rCol]
-            view.invalidate()
-        }
+        // TODO uncomment following line once PR#31 is merged
+//        coroutineScope.launch { categories = datasetInterface.getCategories() }
+        // TODO remove following once PR#31 merged
+        val fork = DummyCategory("fork")
+        val knife = DummyCategory("knife")
+        val spoon = DummyCategory("spoon")
+        categories = setOf(fork, knife, spoon)
+
+        this.datasetInterface = datasetInterface
+    }
+
+    suspend fun displayNextPicture(activity: Activity, view: ImageView) {
+        // TODO this is a dirty hack, replace with model call
+//            fun gS(id: Int) = view.context.getString(id)
+//            val colorStrings = listOf(gS(R.string.learning_cat_0_descr),
+//                    gS(R.string.learning_cat_1_descr), gS(R.string.learning_cat_2_descr))
+//            val colorInts = listOf(R.color.red, R.color.green, R.color.blue)
+//            val rCol = Random.nextInt(from = 0, until = 2)
+
+        // safety check to be sure there is no race condition
+        if(!this::categories.isInitialized) return
+        val rndCat = categories.random()
+        datasetInterface.getPicture(rndCat)?.displayOn(activity, view)
+        view.contentDescription = rndCat.name
+        view.invalidate()
     }
 }
--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/view/LearningActivity.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/view/LearningActivity.kt
@@ -1,24 +1,126 @@
 package com.github.HumanLearning2021.HumanLearningApp.view
 
+import android.content.ClipData
+import android.content.ClipDescription
 import android.os.Bundle
+import android.util.Log
+import android.view.DragEvent
+import android.view.MotionEvent
+import android.view.View
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.github.HumanLearning2021.HumanLearningApp.R
+import com.github.HumanLearning2021.HumanLearningApp.model.DummyDatasetInterface
+import com.github.HumanLearning2021.HumanLearningApp.presenter.DummyUIPresenter
 import com.github.HumanLearning2021.HumanLearningApp.presenter.LearningPresenter
+import kotlinx.coroutines.launch
 
 class LearningActivity : AppCompatActivity() {
+
+    private val dummyPres = DummyUIPresenter()
+    private val learningPresenter = LearningPresenter(DummyDatasetInterface(), lifecycleScope)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_learning)
 
-        findViewById<ImageView>(R.id.learning_cat_0)
-                .setOnDragListener(LearningPresenter.targetOnDragListener)
-        findViewById<ImageView>(R.id.learning_cat_1)
-                .setOnDragListener(LearningPresenter.targetOnDragListener)
-        findViewById<ImageView>(R.id.learning_cat_2)
-                .setOnDragListener(LearningPresenter.targetOnDragListener)
+        initTargetCategory(R.id.learning_cat_0, "knife")
+        initTargetCategory(R.id.learning_cat_1, "spoon")
+        initTargetCategory(R.id.learning_cat_2, "fork")
 
-        findViewById<ImageView>(R.id.learning_im_to_sort)
-                .setOnTouchListener(LearningPresenter::onImageToSortTouched)
+        initImageToSort(R.id.learning_im_to_sort, "fork")
+
+    }
+
+    private fun initImageView(catIvId: Int, catName: String): ImageView {
+        val catIv = findViewById<ImageView>(catIvId)
+        catIv.contentDescription = catName
+        lifecycleScope.launch {
+            dummyPres.getPicture(catName)?.displayOn(this@LearningActivity, catIv)
+        }
+        return catIv
+    }
+
+    private fun initImageToSort(catIvId: Int, catName: String) {
+        initImageView(catIvId, catName).setOnTouchListener(LearningActivity::onImageToSortTouched)
+    }
+
+    private fun initTargetCategory(catIvId: Int, catName: String) {
+        initImageView(catIvId, catName).setOnDragListener(targetOnDragListener)
+    }
+
+    private val opaque = 1.0f
+    private val halfOpaque = opaque / 2
+    private val targetOnDragListener = View.OnDragListener { v, event ->
+        when (event.action) {
+            DragEvent.ACTION_DRAG_STARTED -> true
+            DragEvent.ACTION_DRAG_ENTERED -> dragEnteredCallback(v)
+            DragEvent.ACTION_DRAG_LOCATION -> true
+            DragEvent.ACTION_DRAG_EXITED -> dragExitedCallback(v)
+            DragEvent.ACTION_DROP -> dropCallback(event, v)
+            DragEvent.ACTION_DRAG_ENDED -> dragEndedCallback(event)
+            else -> {
+                Log.e("targetOnDragListener", "Unkown action type")
+                false
+            }
+        }
+    }
+
+    private fun dragEndedCallback(event: DragEvent) = if (event.result) {
+        when (val iv = event.localState) {
+            is ImageView ->
+                lifecycleScope.launch {
+                    learningPresenter.displayNextPicture(this@LearningActivity, iv)
+                }
+            else -> throw IllegalStateException(
+                "The local state of the drag " +
+                        "and drop should be of type ImageView"
+            )
+        }
+        true
+    } else {
+        false
+    }
+
+    private fun dropCallback(event: DragEvent, v: View): Boolean {
+        val item: ClipData.Item = event.clipData.getItemAt(0)
+        v.alpha = opaque
+        v.invalidate()
+        Log.d("dropCallback", "${item.text} vs ${v.contentDescription}")
+        return item.text == v.contentDescription
+    }
+
+    private fun dragExitedCallback(v: View): Boolean {
+        v.alpha = opaque
+        v.invalidate()
+        return true
+    }
+
+    private fun dragEnteredCallback(v: View): Boolean {
+        v.alpha = halfOpaque
+        v.invalidate() // to force redraw
+        return true
+    }
+
+    companion object {
+        fun onImageToSortTouched(view: View, event: MotionEvent): Boolean {
+            val clipDataLabel = "My Clip Data"
+            return when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    val item = ClipData.Item(view.contentDescription)
+                    val dragData = ClipData(
+                        clipDataLabel,
+                        arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
+                        item
+                    )
+                    val shadow = View.DragShadowBuilder(view)
+                    view.startDragAndDrop(dragData, shadow, view, 0)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 }
