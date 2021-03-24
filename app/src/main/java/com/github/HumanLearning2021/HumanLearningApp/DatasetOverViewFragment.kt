9eb5bb8fcd0a921c9265d206f8926ae222f68289
Marouane Jaakik <marouanejaakik@Marouanes-MacBook-Pro.local>
2021-03-24 11:44:47 +0100

    Made DataSetNames fragement

---
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/DatasetOverViewFragment.kt
@@ -0,0 +1,74 @@
+package com.github.HumanLearning2021.HumanLearningApp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.github.HumanLearning2021.HumanLearningApp.dummy.DummyContent
+
+/**
+ * A fragment representing a list of Items.
+ */
+class DatasetOverViewFragment : Fragment(), MyDatasetRecyclerViewAdapter.OnItemClickListener {
+
+    private var columnCount = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+    //
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_dataset_over_view_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                // this creates a vertical scrolling list
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = MyDatasetRecyclerViewAdapter(listener = this@DatasetOverViewFragment)
+
+            }
+        }
+        return view
+    }
+
+
+    // customize later
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            DatasetOverViewFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
+    }
+
+    override fun onItemClick(position: Int) {
+       // for now let's show only a toast message to make sure the item is clicked
+        // link with activities later
+        Toast.makeText(this.context, "Item $position clicked", Toast.LENGTH_SHORT).show()
+
+    }
+}
