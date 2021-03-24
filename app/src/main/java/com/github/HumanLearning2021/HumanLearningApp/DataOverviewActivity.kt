8c65572bcc5f6c9877440a43651aa041f07865d6
Marouane Jaakik <marouanejaakik@Marouanes-MacBook-Pro.local>
2021-03-24 15:27:00 +0100

    add fragment to overview activity

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/DataOverviewActivity.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/DataOverviewActivity.kt
@@ -7,5 +7,6 @@ class DataOverviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_overview)
+
     }
 }
