7ffb5e45b72b96617757bb774976b330fdcfde3e
Marouane Jaakik <marouanejaakik@Marouanes-MacBook-Pro.local>
2021-03-24 22:18:34 +0100

    add fragment tests

--- app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/DataOverViewActivityTest.kt
+++ app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/DataOverViewActivityTest.kt
@@ -1,21 +1,27 @@
 package com.github.HumanLearning2021.HumanLearningApp
 
-import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
 
 
 @RunWith(AndroidJUnit4::class)
@@ -24,12 +30,19 @@ class DataOverViewActivityTest {
 
     @get:Rule
     var testRule = ActivityTestRule(DataOverviewActivity::class.java)
-    val intentRule = IntentsTestRule(DataOverviewActivity::class.java)
 
 
     @Test
     fun fragmentIsDisplayedWhenActivityIsLaunched(){
         assertDisplayed(R.id.dataOverview_fragment)
+        assertDisplayed(R.id.dataOverviewButton)
     }
+
+
+
+
+
+
+
 
 }
---
+++ app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/FragmentDataOverviewTest.kt
@@ -0,0 +1,5 @@
+package com.github.HumanLearning2021.HumanLearningApp
+
+class FragmentDataOverviewTest {
+
+}
