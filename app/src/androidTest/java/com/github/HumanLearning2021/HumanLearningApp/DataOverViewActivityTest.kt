00bcd694fb228a070ec98fddb53e3996471d3bc3
Marouane Jaakik <marouanejaakik@Marouanes-MacBook-Pro.local>
2021-03-25 00:03:12 +0100

    more tests

--- app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/DataOverViewActivityTest.kt
+++ app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/DataOverViewActivityTest.kt
@@ -1,6 +1,8 @@
 package com.github.HumanLearning2021.HumanLearningApp
 
+import android.content.Intent
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onData
 import androidx.test.espresso.Espresso.onView
@@ -14,13 +16,17 @@ import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.github.HumanLearning2021.HumanLearningApp.view.DisplayDatasetActivity
+import com.github.HumanLearning2021.HumanLearningApp.view.DisplayImageActivity
 import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
 import org.junit.Before
 
 
@@ -29,7 +35,24 @@ import org.junit.Before
 class DataOverViewActivityTest {
 
     @get:Rule
-    var testRule = ActivityTestRule(DataOverviewActivity::class.java)
+    val activityScenarioRule: ActivityScenarioRule<AddPictureActivity> = ActivityScenarioRule(
+        Intent(
+        ApplicationProvider.getApplicationContext(),
+        DataOverviewActivity::class.java
+    )
+    )
+
+    @Before
+    fun setUp() {
+        Intents.init()
+        val delayBeforeTestStart: Long = 5000
+        TestUtils.waitFor(delayBeforeTestStart)
+    }
+    @After
+    fun cleanUp() {
+        Intents.release()
+        activityScenarioRule.scenario.close()
+    }
 
 
     @Test
@@ -37,6 +60,14 @@ class DataOverViewActivityTest {
         assertDisplayed(R.id.dataOverview_fragment)
         assertDisplayed(R.id.dataOverviewButton)
     }
+
+    @Test
+    fun rightActivityIsStartedAfterCreateButton(){
+        onView(withId(R.id.dataOverviewButton)).perform(click())
+        intended(hasComponent(DataCreationActivity::class.java.name))
+    }
+
+
 
 
 
