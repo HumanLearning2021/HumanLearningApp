3dfc81dabf59f6a755a85cc64e1fc30522176938
Marouane Jaakik <marouanejaakik@Marouanes-MacBook-Pro.local>
2021-03-25 10:51:58 +0100

    fix build

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/DataOverviewActivity.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/DataOverviewActivity.kt
@@ -15,8 +15,6 @@ class DataOverviewActivity : AppCompatActivity() {
 
     fun  create(view: View) {
         val intent_overview = Intent(this, DataCreationActivity::class.java)
-        // visual confirmation to make sure toast message is not displayed over fragment as it is hard to test for toasts
-        Toast.makeText(this.context, "switch", Toast.LENGTH_SHORT).show()
         startActivity(intent_overview)
 
     }
