369fed8c97d68679d800e98aa79c206051aa9ae5
Jonas Konrad <jonas.konrad@epfl.ch>
2021-03-28 22:29:04 +0200

    use @Parcelize annotation to remove boilerplate

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/model/DummyCategorizedPicture.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/model/DummyCategorizedPicture.kt
@@ -7,6 +7,7 @@ import android.os.Parcel
 import android.os.Parcelable
 import android.widget.ImageView
 import com.github.HumanLearning2021.HumanLearningApp.R
+import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 import java.lang.IllegalArgumentException
 
@@ -14,36 +15,13 @@ import java.lang.IllegalArgumentException
  * A picture part of the dummy data set. Can be of any of the following categories: "fork",
  * "knife", "spoon"
  */
+@Parcelize
 data class DummyCategorizedPicture(override val category: Category, val picture: Uri) : CategorizedPicture {
-
-    constructor(parcel: Parcel) : this(
-        parcel.readParcelable(Category::class.java.classLoader)!!,
-        parcel.readParcelable(Uri::class.java.classLoader)!!
-    )
 
     override fun displayOn(activity: Activity, imageView: ImageView) {
         if (category !is DummyCategory) throw IllegalArgumentException("provide a dummy category " +
                 "to the class constructor")
         val inputStream = activity.contentResolver.openInputStream(picture)
         imageView.setImageDrawable(Drawable.createFromStream(inputStream, picture.toString()))
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeParcelable(category, flags)
-        parcel.writeParcelable(picture, flags)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<DummyCategorizedPicture> {
-        override fun createFromParcel(parcel: Parcel): DummyCategorizedPicture {
-            return DummyCategorizedPicture(parcel)
-        }
-
-        override fun newArray(size: Int): Array<DummyCategorizedPicture?> {
-            return arrayOfNulls(size)
-        }
     }
 }
--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/model/DummyCategory.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/model/DummyCategory.kt
@@ -3,6 +3,7 @@ package com.github.HumanLearning2021.HumanLearningApp.model
 import android.net.Uri
 import android.os.Parcel
 import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
 
 /**
@@ -10,36 +11,12 @@ import android.os.Parcelable
  *
  * @param name the name of the category (case-sensitive)
  */
-
+@Parcelize
 data class DummyCategory(override val name: String,
                          override val representativePicture: CategorizedPicture?
 ): Category {
     constructor(parcel: Parcel) : this(
         parcel.readString()!!,
         parcel.readParcelable(CategorizedPicture::class.java.classLoader)
-    ) {
-    }
-
-    override fun equals(other: Any?): Boolean {
-        return (other is DummyCategory) && other.name == this.name
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(name)
-        parcel.writeParcelable(representativePicture, flags)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<DummyCategory> {
-        override fun createFromParcel(parcel: Parcel): DummyCategory {
-            return DummyCategory(parcel)
-        }
-
-        override fun newArray(size: Int): Array<DummyCategory?> {
-            return arrayOfNulls(size)
-        }
-    }
+    )
 }
