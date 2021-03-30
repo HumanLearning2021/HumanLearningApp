263174d0cf26a23bf670899a6666fdfd9ade3a50
Niels Lachat <niels.lachat@epfl.ch>
2021-03-30 17:02:15 +0200

    Create LearningDatasetSelection Activity

---
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/view/LearningDatasetSelectionActivity.kt
@@ -0,0 +1,13 @@
+package com.github.HumanLearning2021.HumanLearningApp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.HumanLearning2021.HumanLearningApp.R
+
+class LearningDatasetSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learning_dataset_selection)
+
+    }
+}
