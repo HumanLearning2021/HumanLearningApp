5ff7b8be9f4209df962636013be6a07f9eaa483e
Louis Bettens <louis@bettens.info>
2021-03-31 16:10:44 +0200

    remove GoogleSignInActivity
    log-in widget is now on main screen

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/MainActivity.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/MainActivity.kt
@@ -5,9 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
 import android.view.View
-import com.github.HumanLearning2021.HumanLearningApp.view.GoogleSignInActivity
 import com.github.HumanLearning2021.HumanLearningApp.view.DisplayDatasetActivity
-import com.github.HumanLearning2021.HumanLearningApp.view.LearningActivity
 import com.github.HumanLearning2021.HumanLearningApp.view.LearningSettingsActivity
 
 class MainActivity : AppCompatActivity() {
@@ -28,11 +26,6 @@ class MainActivity : AppCompatActivity() {
 
     fun launchToLearningActivity(view: View) {
         val intent = Intent(this, LearningSettingsActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun launchToLoginFragment(view: View) {
-        val intent = Intent(this, GoogleSignInActivity::class.java)
         startActivity(intent)
     }
 }
