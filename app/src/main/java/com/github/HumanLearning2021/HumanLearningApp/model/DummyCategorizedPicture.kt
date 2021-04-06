75bca82e8a4462937329323f52f18bac91b7de80
mlenw <martinlenweiter@protonmail.com>
2021-04-06 17:38:58 +0200

    make DummyCategorizedPicture a data class again

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/model/DummyCategorizedPicture.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/model/DummyCategorizedPicture.kt
@@ -12,7 +12,7 @@ import java.lang.IllegalArgumentException
  * "knife", "spoon"
  */
 @Parcelize
-class DummyCategorizedPicture(override val category: Category, val picture: Uri) : CategorizedPicture {
+data class DummyCategorizedPicture(override val category: Category, val picture: Uri) : CategorizedPicture {
 
     override fun displayOn(activity: Activity, imageView: ImageView) {
         if (category !is DummyCategory) throw IllegalArgumentException("provide a dummy category " +
@@ -21,17 +21,5 @@ class DummyCategorizedPicture(override val category: Category, val picture: Uri)
         val drawable = Drawable.createFromStream(inputStream, picture.toString())
         imageView.setImageDrawable(drawable)
         imageView.setTag(drawable)
-    }
-
-    // Have to override these 2 and not make it a data class because of DummyDatabaseService's initialization:
-    // A category depends on a rep pic; and a rep pic depends on a category. Making CategorizedPicture
-    // a data class led to an endless loop. If someone has a more elegant solution, would be nice,
-    // but otherwise it's only the Dummy implementation
-    override fun equals(other: Any?): Boolean {
-        return other is DummyCategorizedPicture && other.category == category && other.picture == picture
-    }
-
-    override fun hashCode(): Int {
-        return 17 + 31*category.hashCode() + 31*picture.hashCode()
     }
 }
