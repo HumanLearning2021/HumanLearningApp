3013a8f040f8f9666160ad554bd08ac64637d1a3
mlenw <martinlenweiter@protonmail.com>
2021-04-09 16:54:06 +0200

    use database service again for testing purposes

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/presenter/LearningPresenter.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/presenter/LearningPresenter.kt
@@ -16,7 +16,43 @@ class DummyLearningPresenter(
     private val learningMode: LearningMode
 ) {
     private var previousCategory : Category? = null
+    private val databaseService = DummyDatabaseService()
+
+    suspend fun displayNextPicture(activity: Activity, view: ImageView) {
+        /*
+        TODO: should the activity be a property of the class instead? Since the presenter and the activity have a 1-1 relationship
+         */
+        val cats = databaseService.getCategories()
+        var rndCat: Category?
+        do {
+            rndCat = cats.random()
+        } while (previousCategory == rndCat)
+        Log.d("displayNextPicture", "previous : $previousCategory, curr : $rndCat")
+        previousCategory = rndCat
+
+        val nextPicture = when(learningMode){
+            LearningMode.REPRESENTATION -> databaseService.getPicture(rndCat!!)
+            LearningMode.PRESENTATION -> databaseService.getRepresentativePicture(rndCat!!.name)
+        }
+        nextPicture!!.displayOn(activity, view)
+        view.contentDescription = rndCat!!.name
+        view.invalidate()
+    }
+
+
+    suspend fun displayTargetPicture(activity: Activity, view: ImageView, categoryName: String){
+            databaseService.getPicture(databaseService.getCategory(categoryName)!!)?.displayOn(activity, view)
+    }
+}
+
+
+/*
+class DummyLearningPresenter(
+    private val learningMode: LearningMode
+) {
+    private var previousCategory : Category? = null
     private val databaseManagement = DummyDatabaseManagement.staticDummyDatabaseManagement
+
 
 
     suspend fun displayNextPicture(activity: Activity, view: ImageView) {
@@ -46,3 +82,4 @@ class DummyLearningPresenter(
         databaseManagement.getRepresentativePicture(categoryName)?.displayOn(activity, view)
     }
 }
+ */
