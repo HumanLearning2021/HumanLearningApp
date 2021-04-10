ef3fbce097ecaabe2bfb9e5c043759f3dc3dae9d
mlenw <martinlenweiter@protonmail.com>
2021-04-10 18:48:57 +0200

    clean up code

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/presenter/LearningPresenter.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/presenter/LearningPresenter.kt
@@ -36,43 +36,6 @@ class DummyLearningPresenter(
             LearningMode.REPRESENTATION -> databaseManagement.getPicture(rndCat!!)
             LearningMode.PRESENTATION -> databaseManagement.getRepresentativePicture(rndCat!!.name)
         }
-        nextPicture!!.displayOn(activity, view)
-        view.contentDescription = rndCat!!.name
-        view.invalidate()
-    }
-
-
-    suspend fun displayTargetPicture(activity: Activity, view: ImageView, categoryName: String){
-        databaseManagement.getPicture(databaseManagement.getCategoryById(categoryName)!!)?.displayOn(activity, view)
-    }
-}
-
-
-/*
-class DummyLearningPresenter(
-    private val learningMode: LearningMode
-) {
-    private var previousCategory : Category? = null
-    private val databaseManagement = DummyDatabaseManagement.staticDummyDatabaseManagement
-
-
-
-    suspend fun displayNextPicture(activity: Activity, view: ImageView) {
-        /*
-        TODO: should the activity be a property of the class instead? Since the presenter and the activity have a 1-1 relationship
-         */
-        val cats = databaseManagement.getCategories()
-        var rndCat: Category?
-        do {
-            rndCat = cats.random()
-        } while (previousCategory == rndCat)
-        Log.d("displayNextPicture", "previous : $previousCategory, curr : $rndCat")
-        previousCategory = rndCat
-
-        val nextPicture = when(learningMode){
-            LearningMode.REPRESENTATION -> databaseManagement.getPicture(rndCat!!)
-            LearningMode.PRESENTATION -> databaseManagement.getRepresentativePicture(rndCat!!.name)
-        }
 
         nextPicture!!.displayOn(activity, view)
         view.contentDescription = rndCat!!.name
@@ -84,4 +47,3 @@ class DummyLearningPresenter(
         databaseManagement.getRepresentativePicture(categoryName)?.displayOn(activity, view)
     }
 }
- */
