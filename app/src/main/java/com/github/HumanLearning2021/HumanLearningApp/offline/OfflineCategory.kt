2303d33cea5da8a9a18474b610a10f987b2d5053
jonaskon <71401115+jonaskon@users.noreply.github.com>
2021-04-12 11:52:23 +0200

    remove offline implementation stubs

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/offline/OfflineCategory.kt
+++
@@ -1,8 +0,0 @@
-package com.github.HumanLearning2021.HumanLearningApp.offline
-
-import com.github.HumanLearning2021.HumanLearningApp.model.Category
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-class OfflineCategory(override val id: String, override val name: String) : Category {
-}
