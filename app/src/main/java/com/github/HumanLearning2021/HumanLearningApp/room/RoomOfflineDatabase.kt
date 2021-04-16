11ffe82e93bb24272e21351d07368fe38aa03e91
jonaskon <71401115+jonaskon@users.noreply.github.com>
2021-04-16 11:07:03 +0200

    add singleton companion object to roomdatabase

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/room/RoomOfflineDatabase.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/room/RoomOfflineDatabase.kt
@@ -1,6 +1,8 @@
 package com.github.HumanLearning2021.HumanLearningApp.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
@@ -14,4 +16,22 @@ abstract class RoomOfflineDatabase : RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
     abstract fun datasetDao(): DatasetDao
     abstract fun userDao(): UserDao
+
+    companion object {
+        // Singleton prevents multiple instances of the database opening at the same time
+        @Volatile
+        private var INSTANCE: RoomOfflineDatabase? = null
+
+        fun getDatabase(context: Context): RoomOfflineDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    RoomOfflineDatabase::class.java,
+                    "general_offline_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
