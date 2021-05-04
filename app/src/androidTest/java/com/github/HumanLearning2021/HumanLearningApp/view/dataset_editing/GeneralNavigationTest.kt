25524352a9bb3b9ad161154da78703675b8aaee6
mlenw <martinlenweiter@protonmail.com>
2021-05-04 20:12:27 +0200

    add support for up navigation

--- app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/view/dataset_editing/GeneralNavigationTest.kt
+++ app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/view/dataset_editing/GeneralNavigationTest.kt
@@ -1,11 +1,7 @@
 package com.github.HumanLearning2021.HumanLearningApp.view.dataset_editing
 
 import android.content.Intent
-import android.os.Bundle
-import androidx.core.os.bundleOf
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.testing.TestNavHostController
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
@@ -14,31 +10,33 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withContentDescription
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import com.example.android.architecture.blueprints.todoapp.launchFragmentInHiltContainer
 import com.github.HumanLearning2021.HumanLearningApp.R
-import com.github.HumanLearning2021.HumanLearningApp.TestUtils
 import com.github.HumanLearning2021.HumanLearningApp.hilt.DatabaseManagementModule
 import com.github.HumanLearning2021.HumanLearningApp.hilt.Demo2Database
-import com.github.HumanLearning2021.HumanLearningApp.model.*
-import com.github.HumanLearning2021.HumanLearningApp.view.HomeFragmentDirections
+import com.github.HumanLearning2021.HumanLearningApp.model.DatabaseManagement
+import com.github.HumanLearning2021.HumanLearningApp.model.DummyDatabaseManagement
+import com.github.HumanLearning2021.HumanLearningApp.model.DummyDatabaseService
 import com.github.HumanLearning2021.HumanLearningApp.view.MainActivity
 import com.github.HumanLearning2021.HumanLearningApp.view.dataset_list_fragment.DatasetListRecyclerViewAdapter
 import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
-import org.junit.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mockito
 
 @UninstallModules(DatabaseManagementModule::class)
 @HiltAndroidTest
 @RunWith(AndroidJUnit4::class)
-class AddPictureNavigationTest {
+class GeneralNavigationTest {
+
     @get:Rule
     val hiltRule = HiltAndroidRule(this)
 
@@ -54,17 +52,6 @@ class AddPictureNavigationTest {
     @Demo2Database
     val dbManagement: DatabaseManagement = DummyDatabaseManagement(DummyDatabaseService())
 
-    private val catSet = setOf<Category>(
-        DummyCategory("cat1", "cat1"),
-        DummyCategory("cat2", "cat2"),
-        DummyCategory("cat3", "cat3"),
-    )
-
-    val categories = catSet.toTypedArray()
-
-    val navController = TestNavHostController(
-        ApplicationProvider.getApplicationContext())
-
     @Before
     fun setup() {
         hiltRule.inject()
@@ -78,45 +65,21 @@ class AddPictureNavigationTest {
     }
 
 
-
-
     @Test
-    fun navigateToChoose() {
-        navigateToAddPictureActivity()
+    fun navigateToDisplayDatasetAndThenNavigateUpGoesToDatasetsOverview(){
+        navigateToDisplayDataset()
+        assertCurrentFragmentIsCorrect(R.id.displayDatasetFragment)
+        onView(withContentDescription("Navigate up")).perform(click());
+        assertCurrentFragmentIsCorrect(R.id.datasetsOverviewFragment)
 
-        onView(withId(R.id.select_existing_picture)).perform(click())
-
-        activityScenarioRule.scenario.onActivity {
-            var currentFragmentContainer =
-                it.supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
-            val currentFragment = currentFragmentContainer?.findNavController()?.currentDestination
-            assert(currentFragment?.id == R.id.selectPictureFragment)
-        }
     }
 
     @Test
-    fun navigateToCamera() {
-        navigateToAddPictureActivity()
-        Espresso.onView(ViewMatchers.withId(R.id.use_camera))
-            .perform(ViewActions.click())
+    fun createDatasetAndNavigateUpGoesToDatasetsOverview(){
 
-        activityScenarioRule.scenario.onActivity {
-            var currentFragmentContainer =
-                it.supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
-            val currentFragment = currentFragmentContainer?.findNavController()?.currentDestination
-            assert(currentFragment?.id == R.id.takePictureFragment)
-        }
     }
 
-
-    private fun launchFragment(args:Bundle) {
-        launchFragmentInHiltContainer<DisplayDatasetFragment>(args) {
-            navController.setGraph(R.navigation.nav_graph)
-            Navigation.setViewNavController(requireView(), navController)
-        }
-    }
-
-    private fun navigateToAddPictureActivity() {
+    private fun navigateToDisplayDataset() {
         Espresso.onView(ViewMatchers.withId(R.id.goToDatasetsOverviewButton))
             .perform(ViewActions.click())
         Espresso.onView(ViewMatchers.withId(R.id.DatasetList_list))
@@ -126,9 +89,15 @@ class AddPictureNavigationTest {
                     ViewActions.click()
                 )
             )
-        Espresso.openActionBarOverflowOrOptionsMenu(InstrumentationRegistry.getInstrumentation().targetContext)
-        onView(ViewMatchers.withText(R.string.add_new_picture)).perform(click())
+    }
 
+    private fun assertCurrentFragmentIsCorrect(expected: Int){
+        activityScenarioRule.scenario.onActivity {
+            var currentFragmentContainer =
+                it.supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
+            val currentFragment = currentFragmentContainer?.findNavController()?.currentDestination
+            assert(currentFragment?.id == expected)
+        }
     }
 
 
--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/view/MainActivity.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/view/MainActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.github.HumanLearning2021.HumanLearningApp.R
 import com.github.HumanLearning2021.HumanLearningApp.view.learning.LearningAudioFeedback
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,8 +26,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
-        navController = navHostFragment.findNavController()
+        val navController = navHostFragment.findNavController()
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+        findViewById<Toolbar>(R.id.toolbar)
+            .setupWithNavController(navController, appBarConfiguration)
+    }
 
-        setSupportActionBar(findViewById(R.id.toolbar))
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
