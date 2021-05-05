c29d4876bdd8f6fa1756a842c5ca134471fa95d0
jonaskon <71401115+jonaskon@users.noreply.github.com>
2021-05-05 19:15:35 +0200

    update displayOn

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/offline/OfflineCategorizedPicture.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/offline/OfflineCategorizedPicture.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.widget.ImageView
+import com.bumptech.glide.Glide
 import com.github.HumanLearning2021.HumanLearningApp.model.CategorizedPicture
 import kotlinx.parcelize.Parcelize
 
@@ -11,9 +12,6 @@ import kotlinx.parcelize.Parcelize
 class OfflineCategorizedPicture(override val id: String, override val category: OfflineCategory, val picture: Uri) : CategorizedPicture {
 
     override fun displayOn(activity: Activity, imageView: ImageView) {
-        val inputStream = activity.contentResolver.openInputStream(picture)
-        val drawable = Drawable.createFromStream(inputStream, picture.toString())
-        imageView.setImageDrawable(drawable)
-        imageView.tag = drawable
+        Glide.with(activity).load(picture).into(imageView)
     }
 }
