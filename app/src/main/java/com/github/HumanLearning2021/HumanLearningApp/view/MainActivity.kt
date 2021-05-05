02915ff9c476848c7ad3dbe951b284cd6eb5a268
mlenw <martinlenweiter@protonmail.com>
2021-05-05 14:22:11 +0200

    Up navigation works

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/view/MainActivity.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/view/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import com.github.HumanLearning2021.HumanLearningApp.R
 import com.google.android.material.appbar.CollapsingToolbarLayout
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,13 +20,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val layout = findViewById<CollapsingToolbarLayout>(R.id.collapsing_toolbar_layout)
+
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         navController = navHostFragment.navController
+
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        layout.setupWithNavController(toolbar, navController, appBarConfiguration)
+        setSupportActionBar(toolbar)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 
     override fun onSupportNavigateUp(): Boolean {
