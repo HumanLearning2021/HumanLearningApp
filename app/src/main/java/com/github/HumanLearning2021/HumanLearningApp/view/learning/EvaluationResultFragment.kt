bd20f3d9627a363b285b73d6ed9de533bd02afcb
mlenw <martinlenweiter@protonmail.com>
2021-05-20 14:24:43 +0200

    add graph and navigation, some tests left

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/view/learning/EvaluationResultFragment.kt
+++ app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/view/learning/EvaluationResultFragment.kt
@@ -5,8 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.github.HumanLearning2021.HumanLearningApp.R
 import com.github.HumanLearning2021.HumanLearningApp.databinding.FragmentEvaluationResultBinding
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
 
 class EvaluationResultFragment : Fragment() {
     private var _binding: FragmentEvaluationResultBinding? = null
@@ -24,10 +29,37 @@ class EvaluationResultFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val barChart = binding.barchartEvaluationResult
-        val evaluationResult = args.evaluationResult
+        val successFailureCountPerPhase = args.evaluationResult.successFailureCountPerPhase
 
         barChart.setDrawBarShadow(false)
         barChart.setDrawValueAboveBar(true)
         barChart.setMaxVisibleValueCount(1)
+        barChart.setPinchZoom(false)
+        barChart.setDrawGridBackground(true)
+
+        val barEntries = ArrayList<BarEntry>()
+        for (i in IntRange(0, 5))
+            if (i == 0)
+                barEntries.add(BarEntry(0f, 0f))
+            else {
+                val nSuccess = successFailureCountPerPhase[i].first as Float
+                val nFailures = successFailureCountPerPhase[i].second as Float
+                barEntries.add(BarEntry(i as Float, nSuccess / nFailures))
+            }
+
+
+        val barDataset = BarDataSet(barEntries, getString(R.string.evaluation_result_graph_label))
+        barDataset.color = R.color.blue
+
+        val barData = BarData(barDataset)
+
+        barData.barWidth = 0.9f
+
+        barChart.data = barData
+
+
+        binding.learnAgainButton.setOnClickListener {
+            findNavController().navigate(EvaluationResultFragmentDirections.actionEvaluationResultFragmentToLearningDatasetSelectionFragment())
+        }
     }
 }
