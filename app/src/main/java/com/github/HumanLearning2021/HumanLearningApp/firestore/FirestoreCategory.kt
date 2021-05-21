ee00b1a4bf06f5eef5621c31e7940887c948de07
Louis Bettens <louis@bettens.info>
2021-05-21 09:31:44 +0200

    remove deprecated aliases

--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/firestore/FirestoreCategory.kt
+++
@@ -1,9 +0,0 @@
-package com.github.HumanLearning2021.HumanLearningApp.firestore
-
-import com.github.HumanLearning2021.HumanLearningApp.model.Category
-
-@Deprecated(
-    "merged with superclass",
-    ReplaceWith("Category", "com.github.HumanLearning2021.HumanLearningApp.model.Category"),
-)
-typealias FirestoreCategory = Category
--- app/src/main/java/com/github/HumanLearning2021/HumanLearningApp/model/DummyDataset.kt
+++
@@ -1,4 +0,0 @@
-package com.github.HumanLearning2021.HumanLearningApp.model
-
-@Deprecated("merged with superclass", ReplaceWith("Dataset"))
-typealias DummyDataset = Dataset
