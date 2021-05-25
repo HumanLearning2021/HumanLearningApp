c760ce46177d921967ce587ae6b2da765b69ef14
Louis Bettens <louis@bettens.info>
2021-05-25 13:19:39 +0200

    remove new redundancy

--- app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/hilt.kt
+++ app/src/androidTest/java/com/github/HumanLearning2021/HumanLearningApp/hilt.kt
@@ -2,10 +2,9 @@ package com.github.HumanLearning2021.HumanLearningApp
 
 import android.content.Context
 import androidx.room.Room
-import com.github.HumanLearning2021.HumanLearningApp.hilt.*
-import com.github.HumanLearning2021.HumanLearningApp.model.DatabaseManagement
-import com.github.HumanLearning2021.HumanLearningApp.model.DatabaseService
-import com.github.HumanLearning2021.HumanLearningApp.model.UniqueDatabaseManagement
+import com.github.HumanLearning2021.HumanLearningApp.hilt.FirebaseFirestoreModule
+import com.github.HumanLearning2021.HumanLearningApp.hilt.RoomDatabase
+import com.github.HumanLearning2021.HumanLearningApp.hilt.RoomDatabaseModule
 import com.github.HumanLearning2021.HumanLearningApp.room.RoomOfflineDatabase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
@@ -37,36 +36,6 @@ class FirebaseFirestoreTestModule {
 
 @TestInstallIn(
     components = [SingletonComponent::class],
-    replaces = [DatabaseServiceModule::class],
-)
-@Module
-object DatabaseServiceTestModule {
-    @DummyDatabase
-    @Provides
-    @Singleton
-    fun provideDummyService() = DatabaseServiceModule.provideDummyService()
-
-    @TestDatabase
-    @Provides
-    fun provideTestService(firestore: FirebaseFirestore) =
-        DatabaseServiceModule.provideTestService(firestore)
-
-    @ScratchDatabase
-    @Provides
-    fun provideScratchService(firestore: FirebaseFirestore) =
-        DatabaseServiceModule.provideScratchService(firestore)
-
-    @OfflineTestDatabase
-    @Provides
-    fun provideTestDatabase(
-        @ApplicationContext context: Context,
-        @GlobalDatabaseManagement uDb: UniqueDatabaseManagement,
-        @RoomDatabase room: RoomOfflineDatabase,
-    ) = DatabaseServiceModule.provideOfflineTestService(context, uDb, room)
-}
-
-@TestInstallIn(
-    components = [SingletonComponent::class],
     replaces = [RoomDatabaseModule::class],
 )
 @Module
@@ -79,41 +48,4 @@ object RoomDatabaseTestModule {
             RoomOfflineDatabase::class.java,
             "general_offline_database"
         ).build()
-}
-
-@TestInstallIn(
-    components = [SingletonComponent::class],
-    replaces = [DatabaseManagementModule::class]
-)
-@Module
-object DatabaseManagementTestModule {
-    @DummyDatabase
-    @Provides
-    fun provideDummyDatabaseManagement(@DummyDatabase db: DatabaseService): DatabaseManagement =
-        DatabaseManagementModule.provideDummyManagement(db)
-
-    @TestDatabase
-    @Provides
-    fun provideTestDatabaseManagement(@TestDatabase db: DatabaseService): DatabaseManagement =
-        DatabaseManagementModule.provideTestService(db)
-
-    @ScratchDatabase
-    @Provides
-    fun provideScratchDatabaseManagement(@ScratchDatabase db: DatabaseService): DatabaseManagement =
-        DatabaseManagementModule.provideScratchService(db)
-
-    @OfflineTestDatabase
-    @Provides
-    fun provideOfflineTestDatabaseManagement(@OfflineTestDatabase db: DatabaseService): DatabaseManagement =
-        DatabaseManagementModule.provideOfflineTestService(db)
-
-    @GlobalDatabaseManagement
-    @Provides
-    fun provideUniqueDatabaseManagement(
-        @ApplicationContext context: Context,
-        @RoomDatabase room: RoomOfflineDatabase,
-        firestore: FirebaseFirestore,
-        @DummyDatabase dummyDb: DatabaseManagement
-    ): UniqueDatabaseManagement =
-        DatabaseManagementModule.provideGlobalDatabaseManagement(context, room, firestore, dummyDb)
 }
